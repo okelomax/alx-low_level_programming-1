5d2d80229dd31993ca09550831453f0dd4cc3504
Edithmary245 <99287943+Edithmary245@users.noreply.github.com>
2022-03-12 17:07:42 +0000

    Update 100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -13,17 +13,16 @@ for (n = 48; n <= 56; n++)
 for (m = 49; m <= 57; m++)
            {
 if (m > n)
-																	            {
-																	putchar(n);
-																	putchar(m);
-																	if (n != 56 || m != 57)
+{
+putchar(n);
+putchar(m);
+if (n != 56 || m != 57)
 																																		                {
 																																		putchar(',');
-																																		putchar(' ');
-																																																			                }
-																																		           }
-																	       }
-   }
+																																		putchar(' ');                                                                                                                                                         																																																			                }
+}																																		           }
+}
+}
 putchar('\n');
 return (0);
 }
