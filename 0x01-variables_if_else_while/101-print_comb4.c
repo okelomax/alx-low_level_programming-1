4753ccd671514a420466029f213ed0b57f956e9c
Edithmary245 <edithmarywaore@gmail.com>
2022-03-14 14:08:27 -0700

    hello c

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+ * main -print all possible diff combination of three digits
+ * return: always 0 (Success)
+ */
+int main(void)
+{
+	int n, m , l;
+	for (n = 48; n < 58; n++)
+	{
+		for (m = 49; m < 58; m++)
+		{
+			for (1 = 50; 1 < 58; 1++)
+			{
+				if (1 > m && m > n)
+				{
+					putchar(n);
+					putchar(m);
+					putchar(1);
+					if (n != 55 || m != 56)
+					{
+						putchar(',');
+						putchar(',')
+					}
+				}
+			}
+		}
+		putchar('\n');
+		return(0);
+	}
+
