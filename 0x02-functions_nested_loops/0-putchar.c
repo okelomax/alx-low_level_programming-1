56bf8b2ac056f41fc977cbca8b551ff6271f93da
Edithmary245 <edithmarywaore@gmail.com>
2022-03-16 13:52:40 -0700

    hello c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,18 +1,20 @@
 #include "main.h"
+
 /**
- * main - Entry point
- * Description: prints 'Holberton \n'
+ * main - print _putchar, followed by a new line
  * Return: Always 0 (success)
  */
 int main(void)
 {
-	char str[] = "_putchar\n";
-	int i = 0;
-	while (str[i] != '\0')
-	{
-		char c = str[i];
-		_putchar(c);
-		i++;
-	}
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 	return (0);
-}
+
+
