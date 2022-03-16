98fcf96b15b2cdd453a1dfd8e10ee7a3c2094cc6
Edithmary245 <edithmarywaore@gmail.com>
2022-03-16 16:11:09 -0700

    added task 0 to project 0x02

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,20 +1,22 @@
 #include "main.h"
 
 /**
- * main - print _putchar, followed by a new line
- * Return: Always 0 (success)
+ *   main -  prints Holberton, followed by a new line
+ *    Return: Always 0 (Success)
  */
 int main(void)
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
+	_putchar('H');
+	_putchar('o');
+	_putchar('l');
+	_putchar('b');
+	_putchar('e');
 	_putchar('r');
+	_putchar('t');
+	_putchar('o');
+	_putchar('n');
 	_putchar('\n');
-	return (0);
 
+	return (0);
+}
 
